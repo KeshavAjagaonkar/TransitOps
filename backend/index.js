86b1010823485d645ea20991b55e6ae95bd78df7
AvatarN03 <jsprashanth003@gmail.com>
2026-07-12 10:55:14 +0530

    Add Prisma configuration

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,21 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
 
-const app = new express();
+import { clerkMiddleware } from "@clerk/express";
 
+const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
 
 
 app.get("/", (req, res) => {
-  res.send("Hello World!fff");
+  res.json(`Welcome to the TransitOps backend website - Hacker !!
+        Congo To catch the Project backend route 👏🏻👏🏻👏🏻👏🏻
+      `);
 });
 
 app.listen(PORT, () => {
